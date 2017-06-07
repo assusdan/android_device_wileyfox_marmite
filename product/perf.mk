fcf2935cb566cfd8ff5fd93549e5bebb35abe51b
BeYkeRYkt <beykerykt@gmail.com>
2017-06-08 03:39:18 +0900

    marmite: use perf_profiles

---
+++ product/perf.mk
@@ -0,0 +1,4 @@
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf-profile0.conf:system/vendor/etc/perf-profile0.conf \
+    $(LOCAL_PATH)/configs/perf-profile1.conf:system/vendor/etc/perf-profile1.conf
